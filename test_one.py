4a651235679b75d4b9bacaf0e003d8c7dd90073b
Sam <sane10@ya.ru>
2022-06-15 21:09:02 +0300

    Test

--- test_one.py
+++ test_one.py
@@ -18,9 +18,9 @@ def test_one_broken(open_google):
     browser.element('[name="q"]').should(be.blank).type(google_search_text).press_enter()
     browser.element('[id="search"]').should(have.text(google_search_results_broken))
 
-def test_one_broken(open_google):
+def test_one_no_result(open_google):
     # browser.open('https://google.com')
     # browser.driver.set_window_size(width=1920, height=1080)
-    browser.element('[name="q"]').should(be.blank).type(google_search_text).press_enter()
-    browser.element('[id="search"]').should(have.text(google_search_results_no_result))
+    browser.element('[name="q"]').should(be.blank).type(google_search_results_no_result).press_enter()
+    browser.element('[id="rcnt"]').should(have.text(google_search_results_no_result))
 
