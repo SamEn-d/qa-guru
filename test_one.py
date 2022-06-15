7c6b74dab8f75e5350dda22ba82a3e8fdcc7726d
Sam <sane10@ya.ru>
2022-06-15 21:05:31 +0300

    Test

--- test_one.py
+++ test_one.py
@@ -4,6 +4,7 @@ from selene import be, have
 google_search_text = 'selene'
 google_search_results = 'Селена - Википедия'
 google_search_results_broken = 'Всё херня, переделывай'
+google_search_results_no_result = 'lksfgbjhopi kdf msvoniguidxf,viugapoivdsjzm fizwepsidz'
 
 def test_one(open_google):
     # browser.open('https://google.com')
@@ -16,4 +17,10 @@ def test_one_broken(open_google):
     # browser.driver.set_window_size(width=1920, height=1080)
     browser.element('[name="q"]').should(be.blank).type(google_search_text).press_enter()
     browser.element('[id="search"]').should(have.text(google_search_results_broken))
+
+def test_one_broken(open_google):
+    # browser.open('https://google.com')
+    # browser.driver.set_window_size(width=1920, height=1080)
+    browser.element('[name="q"]').should(be.blank).type(google_search_text).press_enter()
+    browser.element('[id="search"]').should(have.text(google_search_results_no_result))
 
