08a45a0ec28fe6b8a90a0dd52bfaa132f9318a6e
Sam <sane10@ya.ru>
2022-06-18 14:45:14 +0300

    import time

--- test_hard.py
+++ test_hard.py
@@ -1,7 +1,7 @@
 from selene.support.shared import browser
 from selene import be, have
-# import pytest
-# import time
+import pytest
+import time
 
 def test_hard_form(site_demoqa):
     # browser.open('https://demoqa.com/automation-practice-form')
